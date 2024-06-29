570d4d5704deca3dd35787b69cd923bfafec73a2
sachin bagaria <your.email@example.com>
2024-06-29 11:14:08 +0530

    add new folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import ContactForm from './components/ContactForm'
 
-function App() {
- 
-  const [name , setName] = useState("");
-  const [message , setMessage] = useState("")
-
+const App = () => {
   return (
-    <>
-     <h1>Contact form</h1>
-     <form name="contact" method="POST" >
-  <input type="hidden" name="form-name" value="contact" />
-  <div className="input">
-    <label htmlFor="name">Name</label>
-    <input
-      type="text"
-      id="name"
-      name="name"
-      placeholder="Your Name"
-      value={name}
-      onChange={(e) => setName(e.target.value)}
-    />
-  </div>
-  <div className="input">
-    <label htmlFor="message">Message</label>
-    <textarea
-      name="message"
-      id="message"
-      placeholder='your message'
-      value={message}
-      onChange={(e) => setMessage(e.target.value)}
-    ></textarea>
-  </div>
-  <button type='submit'>Send Message</button>
-</form>
-
-    </>
+    <div>
+      <ContactForm/>
+    </div>
   )
 }
 
---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+const ContactForm = () => {
+    const [name , setName] = useState("");
+    const [message , setMessage] = useState("")
+  
+    return (
+      <>
+       <h1>Contact form</h1>
+       <form name="contact" method="POST" >
+    <input type="hidden" name="form-name" value="contact" />
+    <div className="input">
+      <label htmlFor="name">Name</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        placeholder="Your Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+    </div>
+    <div className="input">
+      <label htmlFor="message">Message</label>
+      <textarea
+        name="message"
+        id="message"
+        placeholder='your message'
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      ></textarea>
+    </div>
+    <button type='submit'>Send Message</button>
+  </form>
+</>  
+    )
+}
+
+export default ContactForm
